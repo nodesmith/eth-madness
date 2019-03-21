4342bd93df33723d579654871eaf2bb699d15040
Samm Desmond <samm@spacedust.io>
2019-03-21 13:10:20 -0700

    add use-context command

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
 IMAGE_NAME=nodesmith/eth-madness:mens-$TRAVIS_TAG
 docker tag nodesmith/eth-madness:latest $IMAGE_NAME
 docker push $IMAGE_NAME
+kubectl config use-context aws-us-east
 kubectl get pods -l app=eth-madness
 
 PATCH_TEMPLATE=$'\'{"spec": {"template": {"spec": {"containers":[{"name":"app","image":"IMAGE_NAME"}]}}}}\''
