e44db93fc3fae9f41112d8ce40c4941fc4366768
Samm Desmond <samm@spacedust.io>
2019-03-21 13:32:39 -0700

    maybe?

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 set -ex
 
 docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
-IMAGE_NAME=nodesmith/eth-madness:mens-$TRAVIS_TAG
+IMAGE_NAME=nodesmith/eth-madness:mens-$TRAVIS_COMMIT
 docker tag nodesmith/eth-madness:latest $IMAGE_NAME
 docker push $IMAGE_NAME
 kubectl config use-context aws-us-east
-kubectl get pods -l app=eth-madness
 
 PATCH_TEMPLATE=$'\'{"spec": {"template": {"spec": {"containers":[{"name":"app","image":"IMAGE_NAME"}]}}}}\''
 PATCH=$(echo ${PATCH_TEMPLATE/IMAGE_NAME/$IMAGE_NAME})
