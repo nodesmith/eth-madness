238fdd408c1479772bd6cdb93ecb7a04d44bf9bb
Samm Desmond <samm@spacedust.io>
2019-03-21 12:33:46 -0700

    Add travis

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+set -ex
+
+IMAGE_NAME=nodesmith/eth-madness:mens-$TRAVIS_TAG
+docker tag nodesmith/eth-madness:latest $IMAGE_NAME
+docker push $IMAGE_NAME
+kubectl get pods -l app=eth-madness
+
+PATCH_TEMPLATE=$'\'{"spec": {"template": {"spec": {"containers":[{"name":"app","image":"IMAGE_NAME"}]}}}}\''
+PATCH=$(echo ${PATCH_TEMPLATE/IMAGE_NAME/$IMAGE_NAME})
+CMD="kubectl patch deployment eth-madness -p $PATCH"
+eval $CMD
+kubectl get pods -l app=eth-madness
