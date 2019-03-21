d86c608defccb063b05ce39842069e321366479e
Samm Desmond <samm@spacedust.io>
2019-03-21 12:58:23 -0700

    Update node version and add secrets

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 set -ex
 
+docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
 IMAGE_NAME=nodesmith/eth-madness:mens-$TRAVIS_TAG
 docker tag nodesmith/eth-madness:latest $IMAGE_NAME
 docker push $IMAGE_NAME
