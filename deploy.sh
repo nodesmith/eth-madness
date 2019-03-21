cbd53cbe0d2261293225c6fb832291e1059027e8
Samm Desmond <samm@spacedust.io>
2019-03-21 14:08:38 -0700

    add kubeconfig flag

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@ docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
 IMAGE_NAME=nodesmith/eth-madness:mens-$TRAVIS_COMMIT
 docker tag nodesmith/eth-madness:latest $IMAGE_NAME
 docker push $IMAGE_NAME
-kubectl config use-context aws-us-east
+kubectl config use-context aws-us-east --kubeconfig $TRAVIS_BUILD_DIR/kubeconfig
 
 PATCH_TEMPLATE=$'\'{"spec": {"template": {"spec": {"containers":[{"name":"app","image":"IMAGE_NAME"}]}}}}\''
 PATCH=$(echo ${PATCH_TEMPLATE/IMAGE_NAME/$IMAGE_NAME})
-CMD="kubectl patch deployment eth-madness -p $PATCH"
+CMD="kubectl patch deployment eth-madness --kubeconfig $TRAVIS_BUILD_DIR/kubeconfig -p $PATCH"
 eval $CMD
-kubectl get pods -l app=eth-madness
+kubectl get pods -l app=eth-madness --kubeconfig $TRAVIS_BUILD_DIR/kubeconfig
